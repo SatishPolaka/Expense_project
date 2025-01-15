8cad72bfffdbcf1beb28eaec6d4177b9e8ae6959
SatishPolaka <satishpolaka@gmail.com>
2025-01-15 19:29:52 +0530

    addition

--- frontend.sh
+++ frontend.sh
@@ -70,7 +70,7 @@ unzip /tmp/frontend.zip &>>$Log_File_Name
 
 
 #vim /etc/nginx/default.d/expense.conf
-cp /home/ec2-user/Expense_project/frontend.conf /etc/nginx/default.d/expense.conf
+mv /home/ec2-user/Expense_project/frontend.conf /etc/nginx/default.d/expense.conf
 
 
 systemctl restart nginx &>>$Log_File_Name
